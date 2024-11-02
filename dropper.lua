6e14ae653665c4b503e55b78bd76e6b0f61d1468
keano <keanoserver@gmail.com>
2024-11-03 00:30:57 +0100

    Update dropper.lua

--- dropper.lua
+++ dropper.lua
@@ -3,8 +3,8 @@
 local DropperTemplate = game.ServerStorage:WaitForChild('DropperTemplate')
 
 while task.wait(2) do
-    local NewDropperPart = DropperTemplate:Clone()
-    NewDropperPart.CFrame = script.Parent.Drop.CFrame
-    NewDropperPart:SetAttribute('CashToGive', script.Parent:GetAttribute('CashPerDrop'))
-    NewDropperPart.Parent = script.Parent.DropperParts
+	local NewDropperPart = DropperTemplate:Clone()
+	NewDropperPart.CFrame = script.Parent.Drop.CFrame - Vector3.new(0,2,0)
+	NewDropperPart:SetAttribute('CashToGive', script.Parent:GetAttribute('CashPerDrop'))
+	NewDropperPart.Parent = script.Parent.DropperParts
 end
